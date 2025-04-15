93d33226f6d895ed12c0744459e28ac3b2f25641
KIM TAEHYUN <tanggu01@connect.hku.hk>
2025-04-15 14:11:30 +0800

    update repository test with sql file

--- src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -3,51 +3,30 @@ package com.example.demo.repository;
 import com.example.demo.model.UserStatus;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@ExtendWith(SpringExtension.class)
-//@TestPropertySource("classpath:test-application.properties")
+@ExtendWith(SpringExtension.class)
+@TestPropertySource("classpath:test-application.properties")
 @DataJpaTest(showSql = true)
+@Sql("/sql/user-repository-test-data.sql")
 public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
     @Test
-    void UserRepository_가_제대로_연결되었다() {
-        // given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setEmail("th@gmail.com");
-        userEntity.setAddress("hk");
-        userEntity.setNickname("thkim");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaa");
-
-        //when
-        UserEntity result = userRepository.save(userEntity);
-
-        // then
-        assertThat(result.getId()).isNotNull();
-    }
-
-    @Test
     void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() {
         // given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("th@gmail.com");
-        userEntity.setAddress("hk");
-        userEntity.setNickname("thkim");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaa");
-
         // when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
 
         // then
@@ -57,21 +36,11 @@ public class UserRepositoryTest {
     @Test
     void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
         // given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("th@gmail.com");
-        userEntity.setAddress("hk");
-        userEntity.setNickname("thkim");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaa");
-
         // when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
 
         // then
-//        assertThat(result.isPresent()).isFalse();
-        assertThat(result.isEmpty()).isTrue(); // better
+        assertThat(result.isEmpty()).isTrue();
 
     }
 
@@ -79,16 +48,7 @@ public class UserRepositoryTest {
     @Test
     void findByEmailAndStatus_로_유저_데이터를_찾아올_수_있다() {
         // given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("th@gmail.com");
-        userEntity.setAddress("hk");
-        userEntity.setNickname("thkim");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaa");
-
         // when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByEmailAndStatus("th@gmail.com", UserStatus.ACTIVE);
 
         // then
@@ -98,25 +58,13 @@ public class UserRepositoryTest {
     @Test
     void findByEmailAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
         // given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("th@gmail.com");
-        userEntity.setAddress("hk");
-        userEntity.setNickname("thkim");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaa");
-
         // when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByEmailAndStatus("th@gmail.com", UserStatus.PENDING);
 
         // then
-//        assertThat(result.isPresent()).isFalse();
-        assertThat(result.isEmpty()).isTrue(); // better
+        assertThat(result.isEmpty()).isTrue();
 
     }
-
-
 
 
 
---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into users (id, email, nickname, address, certification_code, status, last_login_at)
+values (1, 'th@gmail.com', 'thkim', 'hk', 'aaa', 'ACTIVE', 0);
